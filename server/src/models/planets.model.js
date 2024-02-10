426f44bb78e953691af7029d774479b4c3bc7503
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2024-02-10 20:47:22 +0530

    initial commit

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const fs = require("fs");
+
+const { parse } = require("csv-parse");
+const habitablePlanets = [];
+
+function isHabitable(planet) {
+  return (
+    planet.koi_disposition === "CONFIRMED" &&
+    planet.koi_insol > 0.36 &&
+    planet.koi_insol < 1.11 &&
+    planet.koi_prad < 1.6
+  );
+}
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(
+      path.join(__dirname, "..", "..", "data", "kepler_data.csv")
+    )
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      ) // pipe is supposed to connect a readable stream source to a writable stream source
+      .on("data", (data) => {
+        if (isHabitable(data)) habitablePlanets.push(data);
+      })
+      .on("error", (err) => {
+        console.log(err);
+        reject(err);
+      })
+      .on("end", () => {
+        // console.log(
+        //   habitablePlanets.map((habitablePlanet) => habitablePlanet.kepler_name)
+        // );
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const app = require("./app");
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+const { loadPlanetsData } = require("./models/planets.model");
+
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log("listening on port");
+  });
+}
+startServer();
