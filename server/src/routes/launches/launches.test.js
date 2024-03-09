603dab5e02bb791451f780ff1f9cfb17b08f0c54
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2024-03-09 23:51:23 +0530

    test cases using superset and jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,40 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test Get /launches", () => {
+  test("It should respond with 200 status", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect(200)
+      .expect("Content-Type", /json/);
+  });
+});
+
+describe("Test Post /launches", () => {
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4,2028",
+  };
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+  test("it should return 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect(201)
+      .expect("Content-Type", /json/);
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(requestDate).toBe(responseDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+  test("it should catch missing required properties", () => {});
+  test("it should catch invalid dates", () => {});
+});
